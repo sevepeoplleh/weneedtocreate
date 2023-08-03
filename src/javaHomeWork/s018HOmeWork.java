c8ca281992216404fff52866d5bd9d78ec649d4a
sevepeoplleh <sevepeoplleh@gmail.com>
2023-08-03 17:40:06 -0400

    third commit

---
+++ src/javaHomeWork/s018HOmeWork.java
@@ -0,0 +1,9 @@
+package javaHomeWork;
+
+public class s018HOmeWork {
+    public static void main(String[] args) {
+        double num1=500.444;
+        long num2=(long)num1;
+        System.out.println(num2);
+    }
+}
